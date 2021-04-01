7ce59da8c6c81e0c134d00958d3ee44f2c20fc73
Phantom <Phantom@DESKTOP-920PA8E>
2021-04-01 16:33:26 -0300

    se crea componente de menu, login, home y productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,19 @@ import {
   GoogleLoginProvider,
   FacebookLoginProvider
 } from 'angularx-social-login';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    HomeComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
@@ -31,11 +39,12 @@ import {
           {
             id: GoogleLoginProvider.PROVIDER_ID,
             provider: new GoogleLoginProvider(
-''            )
+              '884935543099-kr7p9ud4b3uj71jstr43oso4d7ck3diu.apps.googleusercontent.com'
+            )
           },
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('')
+            provider: new FacebookLoginProvider('1122044368166848')
           }
         ]
       } as SocialAuthServiceConfig,
